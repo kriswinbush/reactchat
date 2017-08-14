dadef0a824f749298ab4f519e636cb4fb09246ff
kriswinbush <kris.winbush@gmail.com>
2017-08-14 21:29:39 +0000

    typo in addstream

--- src/components/rc-video/RcVideo.jsx
+++ src/components/rc-video/RcVideo.jsx
@@ -14,8 +14,12 @@ export default class RcVideo extends React.Component {
     this.lgVideoRef;
     this.smVideRef;
 
-    Rx.Observable.fromEvent(this.props.stores.peerStore.peer, 'addStream')
-      .subscribe(event => this.lgVideoRef.srcObject = event.stream);
+    let streamAdded = Rx.Observable.fromEvent(this.props.stores.peerStore.peer, 'addstream')
+    streamAdded.subscribe(event => {
+	console.log(event);
+	console.log(this.lgVideoRef);
+	 this.lgVideoRef.srcObject = event.stream
+	});
 
     navigator.mediaDevices.getUserMedia({audio:true, video: true})
       .then(stream => this.smVideoRef.srcObject = stream)
